7f1b93ea4ccb03c6ccee2b4c2b81508a0acfd155
Okan <orkunokann@gmail.com>
2023-10-28 12:11:31 +0300

    Project created

---
+++ SwiftUI-Crypto/SwiftUI-Crypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Crypto
+//
+//  Created by Okan Orkun on 28.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Crypto/SwiftUI-Crypto/SwiftUI_CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CryptoApp.swift
+//  SwiftUI-Crypto
+//
+//  Created by Okan Orkun on 28.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
