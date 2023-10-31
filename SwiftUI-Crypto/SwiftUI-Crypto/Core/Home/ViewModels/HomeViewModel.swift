fda1401adabb7a27c904bbae3e173dea78fcc689
Okan <orkunokann@gmail.com>
2023-10-31 16:36:03 +0300

    Save user's portfolio to the Core Data

--- SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -17,6 +17,7 @@ class HomeViewModel: ObservableObject {
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
+    private let portfolioDataService = PortfolioDataService()
     private var cancellable = Set<AnyCancellable>()
     
     init() {
@@ -40,6 +41,26 @@ class HomeViewModel: ObservableObject {
                 self?.statistics = returnedState
             }
             .store(in: &cancellable)
+        
+        $allCoins
+            .combineLatest(portfolioDataService.$savedEntities)
+            .map { (coinModel, portfolioEntities) -> [CoinModel] in
+                coinModel
+                    .compactMap { coin in
+                        guard let entity = portfolioEntities.first(where: { $0.coinID == coin.id }) else {
+                            return nil
+                        }
+                        return coin.updateHoldings(amount: entity.amount)
+                    }
+            }
+            .sink { [weak self] returnedCoins in
+                self?.portfolioCoins = returnedCoins
+            }
+            .store(in: &cancellable)
+    }
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        portfolioDataService.updatePortfolio(coin: coin, amount: amount)
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
