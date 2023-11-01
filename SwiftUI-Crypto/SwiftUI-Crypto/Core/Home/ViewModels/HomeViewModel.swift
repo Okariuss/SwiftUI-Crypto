247e6638f4b7e88e0960aec9c25f4099aa564705
Okan <orkunokann@gmail.com>
2023-11-01 20:11:02 +0300

    Sort feature added

--- SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -15,11 +15,16 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var isLoading: Bool = false
     @Published var searchText: String = ""
+    @Published var sortOption: SortOption = .holdings
     
     private let coinDataService = CoinDataService()
     private let marketDataService = MarketDataService()
     private let portfolioDataService = PortfolioDataService()
     private var cancellable = Set<AnyCancellable>()
+    
+    enum SortOption {
+        case rank, rankReversed, holdings, holdingsReversed, price, priceReversed
+    }
     
     init() {
         addSubscribers()
@@ -28,9 +33,9 @@ class HomeViewModel: ObservableObject {
     func addSubscribers() {
         
         $searchText
-            .combineLatest(coinDataService.$allCoins)
+            .combineLatest(coinDataService.$allCoins, $sortOption)
             .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
-            .map(filterCoins)
+            .map(filterAndSortCoins)
             .sink { [weak self] returnedCoins in
                 self?.allCoins = returnedCoins
             }
@@ -40,7 +45,8 @@ class HomeViewModel: ObservableObject {
             .combineLatest(portfolioDataService.$savedEntities)
             .map(mapAllCoinsToPortfolioCoins)
             .sink { [weak self] returnedCoins in
-                self?.portfolioCoins = returnedCoins
+                guard let self = self else { return }
+                self.portfolioCoins = self.sortPortfolioCoinsIfNeeded(coins: returnedCoins)
             }
             .store(in: &cancellable)
         
@@ -67,6 +73,12 @@ class HomeViewModel: ObservableObject {
         HapticManager.notification(type: .success)
     }
     
+    private func filterAndSortCoins(text: String, coins: [CoinModel], sort: SortOption) -> [CoinModel] {
+        var updatedCoins = filterCoins(text: text, coins: coins)
+        sortCoins(sort: sort, coins: &updatedCoins)
+        return updatedCoins
+    }
+    
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
         guard !text.isEmpty else {
             return coins
@@ -78,6 +90,30 @@ class HomeViewModel: ObservableObject {
             return coin.name.lowercased().contains(lowerCaseText) ||
                     coin.symbol.lowercased().contains(lowerCaseText) ||
                     coin.id.lowercased().contains(lowerCaseText)
+        }
+    }
+    
+    private func sortCoins(sort: SortOption, coins: inout [CoinModel]) {
+        switch sort {
+        case .rank, .holdings:
+            coins.sort(by: { $0.rank < $1.rank })
+        case .rankReversed, .holdingsReversed:
+            coins.sort(by: { $0.rank > $1.rank })
+        case .price:
+            coins.sort(by: { $0.currentPrice < $1.currentPrice })
+        case .priceReversed:
+            coins.sort(by: { $0.currentPrice > $1.currentPrice })
+        }
+    }
+    
+    private func sortPortfolioCoinsIfNeeded(coins: [CoinModel]) -> [CoinModel] {
+        switch sortOption {
+        case .holdings:
+            return coins.sorted(by: { $0.currentHoldingValue < $1.currentHoldingValue })
+        case .holdingsReversed:
+            return coins.sorted(by: { $0.currentHoldingValue > $1.currentHoldingValue })
+        default:
+            return coins
         }
     }
     
--- SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/Views/HomeView.swift
+++ SwiftUI-Crypto/SwiftUI-Crypto/Core/Home/Views/HomeView.swift
@@ -113,13 +113,46 @@ extension HomeView {
     
     private var columnTitle: some View {
         HStack {
-            Text("Coin")
+            HStack(spacing: 4) {
+                Text("Coin")
+                    
+                Image(systemName: "chevron.down")
+                    
+                    .opacity(vm.sortOption == .rank || vm.sortOption == .rankReversed ? 1.0 : 0.0)
+                    .rotationEffect(Angle(degrees: vm.sortOption == .rank ? 0 : 180))
+            }
+            .onTapGesture {
+                vm.sortOption != .rank ? (vm.sortOption = .rank) : (vm.sortOption = .rankReversed)
+            }
+            
             Spacer()
             if showPortfolio {
-                Text("Holdings")
+                HStack {
+                    Text("Holdings")
+                        
+                    Image(systemName: "chevron.down")
+                        
+                        .opacity(vm.sortOption == .holdings || vm.sortOption == .holdingsReversed ? 1.0 : 0.0)
+                        .rotationEffect(Angle(degrees: vm.sortOption == .holdings ? 0 : 180))
+                }
+                .onTapGesture {
+                    vm.sortOption != .holdings ? (vm.sortOption = .holdings) : (vm.sortOption = .holdingsReversed)
+                }
+                
             }
-            Text("Price")
-                .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+            HStack(spacing: 4) {
+                Text("Price")
+                    .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+                    
+                Image(systemName: "chevron.down")
+                    
+                    .opacity(vm.sortOption == .price || vm.sortOption == .priceReversed ? 1.0 : 0.0)
+                    .rotationEffect(Angle(degrees: vm.sortOption == .price ? 0 : 180))
+            }
+            .onTapGesture {
+                vm.sortOption != .price ? (vm.sortOption = .price) : (vm.sortOption = .priceReversed)
+            }
+            
             
             Button {
                 withAnimation(.linear(duration: 2.0)) {
